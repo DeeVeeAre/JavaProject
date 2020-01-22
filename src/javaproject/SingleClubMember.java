6c7b6dec8008f341a8e6e05c1467e4ee529fad31
Dennis <dennisvanrossum@gmail.com>
2020-01-22 06:42:06 +0100

    First commit

---
+++ src/javaproject/SingleClubMember.java
@@ -0,0 +1,32 @@
+package javaproject;
+
+public class SingleClubMember extends Member {
+
+    private int club;
+
+    public SingleClubMember(char pMemberType, int pMemberID, String pName, double pFees) {
+        super(pMemberType, pMemberID, pName, pFees);
+        club = pClub;
+    }
+
+    public void setClub(int pClub) { club = pClub; }
+    public int getClub() { return club; }
+
+    @Override
+    public String toString() {
+
+        char[] memberType = {getMemberType()};
+        int[] memberID = {getMemberID()};
+        String[] name = {getName()};
+        double[] fees = {getFees()};
+
+        super.toString();
+
+        int[] club = {getClub()};
+
+        return memberType + ", " + memberID + ", " + name + ", " + fees + ", " + club;
+
+    }
+
+
+}
