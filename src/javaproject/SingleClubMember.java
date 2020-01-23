3ad01414d26447574b3647a112a4b43b34799548
Dennis <dennisvanrossum@gmail.com>
2020-01-23 20:59:26 +0100

    first commit to newly made repo

--- src/javaproject/SingleClubMember.java
+++ src/javaproject/SingleClubMember.java
@@ -4,7 +4,7 @@ public class SingleClubMember extends Member {
 
     private int club;
 
-    public SingleClubMember(char pMemberType, int pMemberID, String pName, double pFees) {
+    public SingleClubMember(char pMemberType, int pMemberID, String pName, double pFees, int pClub) {
         super(pMemberType, pMemberID, pName, pFees);
         club = pClub;
     }
