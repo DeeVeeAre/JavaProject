957df98cf84d352e08f922d6fb30a0b60bb60857
Dennis <dennisvanrossum@gmail.com>
2020-01-23 21:14:12 +0100

    added MultiClubMember class

--- src/javaproject/MultiClubMember.java
+++ src/javaproject/MultiClubMember.java
@@ -1,4 +1,30 @@
 package javaproject;
 
-public class MultiClubMember {
+public class MultiClubMember extends  Member {
+
+    private int membershipPoints;
+
+    public MultiClubMember(char pMemberType, int pMemberID, String pName, double pFees, int pMembershipPoints) {
+        super(pMemberType, pMemberID, pName, pFees);
+        membershipPoints = pMembershipPoints;
+    }
+
+    public void setMembershipPoints(int pMembershipPoints) { membershipPoints = pMembershipPoints; }
+    public int getMembershipPoints() { return membershipPoints; }
+
+    @Override
+    public String toString() {
+
+        char[] memberType = {getMemberType()};
+        int[] memberID = {getMemberID()};
+        String[] name = {getName()};
+        double[] fees = {getFees()};
+
+        super.toString();
+
+        int[] membershipPoints = {getMembershipPoints()};
+
+        return memberType + ", " + memberID + ", " + name + ", " + fees + ", " + membershipPoints;
+    }
+
 }
